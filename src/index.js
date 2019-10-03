546d24b3e9abb9ffbdaa71d1b863a866e470d01b
Alexey Brambalov <brambalov@gmail.com>
2019-10-03 13:14:27 +0100

    Multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let firstNum, secNumber, result;
+
+  firstNum = BigInt(first);
+  secNumber = BigInt(second);
+  result = firstNum * secNumber;
+
+  result = result.toString();
+  return result;
+};
