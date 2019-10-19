3bca57f669ef94d9b017a3d0b5295e3d6923d108
Alexey Brambalov <brambalov@gmail.com>
2019-10-19 23:12:39 +0100

    docs: added better solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 module.exports = function multiply(first, second) {
-  let firstNum, secNumber, result;
+  let aa = first.split('').reverse();
+  let bb = second.split('').reverse();
 
-  firstNum = BigInt(first);
-  secNumber = BigInt(second);
-  result = firstNum * secNumber;
+  let stack = [];
 
-  result = result.toString();
-  return result;
+  for (let i = 0; i < aa.length; i++) {
+    for (let j = 0; j < bb.length; j++) {
+      let m = aa[i] * bb[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+    }
+  }
+
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10;
+    let move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1])
+      stack[i + 1] += move;
+    else if (move != 0)
+      stack[i + 1] = move;
+  }
+
+
+  return stack.reverse().join('');
 };
